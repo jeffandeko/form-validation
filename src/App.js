82dea409172a92547fdc3fdf9c16d4168f9a20d0
jeff andeko <j.andeko.j@gmail.com>
2020-04-18 21:09:34 +0300

    created form for name input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="wrapper">
+          <h1>Create an account</h1>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                className=""
+                placeholder="First Name"
+                name="firstName"
+                noValidate
+                onChange={this.handlechange}
+              />
+            </div>
+          </form>
 
-    </div>
-  );
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
