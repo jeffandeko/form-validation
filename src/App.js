899148658b63fa5db21f3b33674e31d7f0063973
jeff andeko <j.andeko.j@gmail.com>
2020-04-18 22:23:23 +0300

    created app.css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <div className="wrapper">
+        <div className="form-wrapper">
           <h1>Create an account</h1>
           <form onSubmit={this.handleSubmit} noValidate>
             <div className="firstName">
@@ -18,9 +19,43 @@ class App extends Component {
                 onChange={this.handlechange}
               />
             </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                className=""
+                placeholder="Last Name"
+                name="lasttName"
+                noValidate
+                onChange={this.handlechange}
+              />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className=""
+                placeholder="email"
+                name="email"
+                noValidate
+                onChange={this.handlechange}
+              />
+            </div>  <div className="password">
+              <label htmlFor="firstName">Password</label>
+              <input
+                type="text"
+                className=""
+                placeholder="password"
+                name="password"
+                noValidate
+                onChange={this.handlechange}
+              />
+            </div>
           </form>
 
         </div>
+
+
       </div>
     );
   }
